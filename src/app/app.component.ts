20cde098636ba7f92b1e757db170560e90d59aec
Mukul Rathod <mrathod@cninc.com>
2019-05-21 15:11:32 -0400

    Added changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,31 +9,33 @@ import { MenuItem } from 'primeng/api';
 export class AppComponent implements OnInit {
 
   items: MenuItem[];
+  logoutItems: MenuItem[];
 
   ngOnInit() {
       this.items = [
           {
-              label: 'File',
-              items: [{
-                      label: 'New',
-                      icon: 'pi pi-fw pi-plus',
-                      items: [
-                          {label: 'Project'},
-                          {label: 'Other'},
-                      ]
-                  },
-                  {label: 'Open'},
-                  {label: 'Quit'}
-              ]
+              label: 'Dashboard',
+              icon: 'pi pi-fw pi-home',
           },
           {
-              label: 'Edit',
-              icon: 'pi pi-fw pi-pencil',
-              items: [
-                  {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                  {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-              ]
-          }
+              label: 'Team',
+              icon: 'pi pi-fw pi-users',
+          },
+          {
+            label: 'Analytics',
+            icon: 'pi pi-fw pi-chart-bar',
+          },
       ];
+
+      this.logoutItems = [
+        {
+          label: 'Profile',
+          icon: 'pi pi-fw pi-user',
+        },
+        {
+            label: 'Logout',
+            icon: 'pi pi-fw pi-power-off',
+        },
+    ];
   }
 }
